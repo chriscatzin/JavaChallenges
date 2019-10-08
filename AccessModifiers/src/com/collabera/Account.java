f506a6c585c0ff8c6599225bad284179cab53cee
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-10-07 18:35:43 -0700

    Java Daily Challenge

--- AccessModifiers/src/com/collabera/Account.java
+++ AccessModifiers/src/com/collabera/Account.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public class Account {
 
-    public String accountName;
-    public int balance = 0;
-    public ArrayList<Integer> transaction;
+    private String accountName;
+    private int balance = 0;
+    private ArrayList<Integer> transaction;
 
     public Account(String accountName) {
         this.accountName = accountName;
@@ -16,5 +16,37 @@ public class Account {
     public int getBalance() {
         return balance;
     }
+
+    public void deposit(int amount) {
+        if(amount > 0){
+            transaction.add(amount);
+            this.balance += amount;
+            System.out.println(amount + " deposited. " +
+                    "Balance is now " + this.balance);
+        }else{
+            System.out.println("Cannot deposit negative sums");
+        }
+    }
+
+    public void withdraw(int amount){
+        int withdrawal = -amount;
+        if(withdrawal < 0){
+            this.transaction.add(withdrawal);
+            this.balance += withdrawal;
+            System.out.println(amount + " withdrawn. Balance " +
+                               "is now " + this.balance );
+        }else{
+            System.out.println("Cannot withdraw a negative sums.");
+        }
+    }
+
+    public void calculateBalance(){
+        this.balance = 0;
+        for(int i : this.transaction){
+            this.balance += i;
+        }
+        System.out.println("Calculated balance is " + this.balance);
+    }
+
 }
 
--- AccessModifiers/src/com/collabera/Main.java
+++ AccessModifiers/src/com/collabera/Main.java
@@ -3,6 +3,16 @@ package com.collabera;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Account chrisAccount = new Account("Chris");
+	    chrisAccount.deposit(1000);
+	    chrisAccount.withdraw(500);
+	    chrisAccount.withdraw(-200);
+	    chrisAccount.deposit(-20);
+	    chrisAccount.calculateBalance();
+	    chrisAccount.balance = 5000;
+
+        System.out.println("Balance on account is " +
+                chrisAccount.getBalance());
+        chrisAccount.calculateBalance();
     }
 }
