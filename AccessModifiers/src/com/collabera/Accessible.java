cc434b4cd2148ed1091eaa6dcf1ad2ca0d68b839
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-10-07 18:52:24 -0700

    Java Daily Challenge

---
+++ AccessModifiers/src/com/collabera/Accessible.java
@@ -0,0 +1,8 @@
+package com.collabera;
+
+public interface Accessible {
+    int SOME_CONSTANT = 100;
+    public void methodA();
+    void methodB();
+    boolean methodC();
+}
--- AccessModifiers/src/com/collabera/Main.java
+++ AccessModifiers/src/com/collabera/Main.java
@@ -3,16 +3,16 @@ package com.collabera;
 public class Main {
 
     public static void main(String[] args) {
-	    Account chrisAccount = new Account("Chris");
-	    chrisAccount.deposit(1000);
-	    chrisAccount.withdraw(500);
-	    chrisAccount.withdraw(-200);
-	    chrisAccount.deposit(-20);
-	    chrisAccount.calculateBalance();
-	    chrisAccount.balance = 5000;
+		Account chrisAccount = new Account("Chris");
+		chrisAccount.deposit(1000);
+		chrisAccount.withdraw(500);
+		chrisAccount.withdraw(-200);
+		chrisAccount.deposit(-20);
+		chrisAccount.calculateBalance();
+		//chrisAccount.balance = 5000;
 
-        System.out.println("Balance on account is " +
-                chrisAccount.getBalance());
-        chrisAccount.calculateBalance();
+		System.out.println("Balance on account is " + chrisAccount.getBalance());
+		//chrisAccount.transactions.add(4500);
+		chrisAccount.calculateBalance();
     }
 }
