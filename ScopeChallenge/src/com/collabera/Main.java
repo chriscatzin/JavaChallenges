9b5ac02df92c8dfc7189d83074e08650aee49f31
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-10-07 17:24:32 -0700

    Java Daily Challenge

--- ScopeChallenge/src/com/collabera/Main.java
+++ ScopeChallenge/src/com/collabera/Main.java
@@ -3,11 +3,11 @@ package com.collabera;
 public class Main {
 
     public static void main(String[] args) {
-        String privateVar = "this is private to main()";
+        String varFour = "this is private to main()";
 
         ScopeCheck scopeInstance = new ScopeCheck();
-        System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
-        System.out.println(privateVar);
+        System.out.println("scopeInstance varOne is " + scopeInstance.getVarOne());
+        System.out.println(varFour);
 
         scopeInstance.timesTwo();
         System.out.println("***********************************");
