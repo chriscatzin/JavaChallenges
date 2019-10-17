76df3d0b89591ab65299fd218a4db7233ce8aa6b
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-10-16 23:19:14 -0700

    Java Daily Challenge

---
+++ interviewPractice/src/com/collabera/Main.java
@@ -0,0 +1,15 @@
+package com.collabera;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int number = 50;
+
+        for(int i = number; i >= 0; i--){
+            if(i % 2 == 1){
+                System.out.println(i);
+            }
+        }
+    }
+}
