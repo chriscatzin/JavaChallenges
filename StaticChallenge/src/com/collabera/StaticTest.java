144af4682bada4532b0b060feb6648bd9b425b23
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-10-24 23:46:54 -0700

    Java Daily Challenge

--- StaticChallenge/src/com/collabera/StaticTest.java
+++ StaticChallenge/src/com/collabera/StaticTest.java
@@ -3,4 +3,19 @@ package com.collabera;
 // Needs work
 
 public class StaticTest {
+    private static int numInstances = 0;
+    private String name;
+
+    public StaticTest(String name) {
+        this.name = name;
+        numInstances++;
+    }
+
+    public static int getNumInstances() {
+        return numInstances;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
