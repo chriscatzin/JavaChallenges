133128459050ea45a2931a7767bc151943c943d0
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-10-29 22:54:30 -0700

    Java Daily Challenge

--- LinkedListChallenge/src/com/collabera/Main.java
+++ LinkedListChallenge/src/com/collabera/Main.java
@@ -8,6 +8,11 @@ package com.collabera;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Customer customer = new Customer("Chris", 54.96);
+        Customer anotherCustomer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(12.18);
+        System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
     }
 }
