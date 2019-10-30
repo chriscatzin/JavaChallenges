8846134a955b167254e5ce5da57eba56742f47e0
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-10-29 23:51:11 -0700

    Java Daily Challenge

--- LinkedListChallenge/src/com/collabera/Demo.java
+++ LinkedListChallenge/src/com/collabera/Demo.java
@@ -2,6 +2,7 @@ package com.collabera;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Demo {
 
@@ -34,5 +35,26 @@ public class Demo {
             System.out.println("Now visiting " + i.next());
         }
         System.out.println("======================");
+    }
+
+    private static boolean addInOrder(LinkedList<String> linkedList, String newCity){
+        ListIterator<String> stringListIterator = linkedList.listIterator();
+
+        while(stringListIterator.hasNext()){
+            int comparison = stringListIterator.next().compareTo(newCity);
+            if(comparison == 0){
+                System.out.println(newCity + " is already included as a destination");
+                return false;
+            } else if(comparison > 0){
+                // new city should appear before this one
+                stringListIterator.previous();
+                stringListIterator.add(newCity);
+                return true;
+            } else if(comparison < 0){
+                // move on to the next city
+
+            }
+        }
+        stringListIterator.add(newCity);
     }
 }
