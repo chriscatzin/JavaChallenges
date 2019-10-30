acd18d91d2994f6b99919ef33801cdc16f4f9067
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-10-29 22:27:02 -0700

    Java Daily Challenge

---
+++ LinkedListChallenge/src/com/collabera/Main.java
@@ -0,0 +1,13 @@
+package com.collabera;
+
+/************************
+ * Christopher Catzin
+ * Last Modified: October 29, 2019
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
