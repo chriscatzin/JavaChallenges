9d143d2d2931fe6c88345cfa7b11e59376c5dc7b
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-10-29 23:07:40 -0700

    Java Daily Challenge

--- LinkedListChallenge/src/com/collabera/Main.java
+++ LinkedListChallenge/src/com/collabera/Main.java
@@ -1,5 +1,7 @@
 package com.collabera;
 
+import java.util.ArrayList;
+
 /************************
  * Christopher Catzin
  * Last Modified: October 29, 2019
@@ -14,5 +16,22 @@ public class Main {
         anotherCustomer = customer;
         anotherCustomer.setBalance(12.18);
         System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+
+        for(int i = 0; i < intList.size(); i++){
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+        intList.add(1, 2);
+
+        for(int i = 0; i < intList.size(); i++){
+            System.out.println(i + ": " + intList.get(i));
+        }
+
     }
 }
