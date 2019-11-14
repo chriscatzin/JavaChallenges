9c84a136de9da792ee8679f3dd6828d67197d3bd
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-11-13 23:17:23 -0800

    Java Daily Challenge

--- LinkedListChallenge/src/com/collabera/Customer.java
+++ LinkedListChallenge/src/com/collabera/Customer.java
@@ -1,7 +1,7 @@
 package com.collabera;
 
 // Christopher Catzin
-// Modified: November 12, 2019
+// Modified: November 13, 2019
 // This needs some work still.
 
 public class Customer {
--- LinkedListChallenge/src/com/collabera/Demo.java
+++ LinkedListChallenge/src/com/collabera/Demo.java
@@ -1,7 +1,7 @@
 package com.collabera;
 
 // Christopher Catzin
-// Modified: November 12, 2019
+// Modified: November 13, 2019
 
 import java.util.Iterator;
 import java.util.LinkedList;
